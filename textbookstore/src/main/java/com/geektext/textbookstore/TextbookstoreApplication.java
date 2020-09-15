69d59a1318092ee3fbe026682c0118c94b72f30e
server107 <71307167+server107@users.noreply.github.com>
2020-09-15 17:23:35 -0400

    Add files via upload

---
+++ textbookstore/src/main/java/com/geektext/textbookstore/TextbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.geektext.textbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextbookstoreApplication.class, args);
+	}
+
+}
---
+++ textbookstore/src/test/java/com/geektext/textbookstore/TextbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geektext.textbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
